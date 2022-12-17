e9e830efa05cf3bb009f715586f48545018cfd34
Ivan0503 <ivanklochkov90@mail.ru>
2022-12-17 22:11:11 +0300

    add test_fixture

---
+++ test_unittest_authorization.py
@@ -0,0 +1,84 @@
+import unittest
+
+class TestAbs(unittest.TestCase):
+    def test_abs1(self):
+
+        from selenium import webdriver
+        from selenium.webdriver.common.by import By
+        import time
+
+        link = "http://suninjuly.github.io/registration1.html"  # тест пройдёт
+        browser = webdriver.Chrome()
+        browser.get(link)
+
+        # Ваш код, который заполняет обязательные поля
+        input1 = browser.find_element(By.CSS_SELECTOR, '.first_block .first')  # или '[placeholder="Input your first name"]' или ".first_block .form-control.first"
+        input1.send_keys("Ivan")
+        input2 = browser.find_element(By.CSS_SELECTOR, '[placeholder="Input your last name"]')  # или ".first_block .second" или ".first_block .form-control.second"
+        input2.send_keys("Petrov")
+        input3 = browser.find_element(By.CSS_SELECTOR, '[placeholder="Input your email"]')  # или ".first_block .third" или ".first_block .form-control.third"
+        input3.send_keys("ivanpetrov@mail.ru")
+
+        time.sleep(5)
+        # Отправляем заполненную форму
+        button = browser.find_element(By.CSS_SELECTOR, "button.btn")
+        button.click()
+
+        # Проверяем, что смогли зарегистрироваться
+        # ждем загрузки страницы
+        time.sleep(2)
+
+        # находим элемент, содержащий текст
+        welcome_text_elt = browser.find_element(By.TAG_NAME, "h1")
+        # записываем в переменную welcome_text текст из элемента welcome_text_elt
+        welcome_text = welcome_text_elt.text
+
+        # с помощью assert проверяем, что ожидаемый текст совпадает с текстом на странице сайта
+        self.assertEqual('Congratulations! You have successfully registered!', welcome_text), "NoSuchElementException"
+
+        # ожидание чтобы визуально оценить результаты прохождения скрипта
+        time.sleep(5)
+        # закрываем браузер после всех манипуляций
+        browser.quit()
+
+    def test_abs2(self):
+        from selenium import webdriver
+        from selenium.webdriver.common.by import By
+        import time
+
+        link = "http://suninjuly.github.io/registration2.html" #тест упадет с ошибкой
+        browser = webdriver.Chrome()
+        browser.get(link)
+
+        # Ваш код, который заполняет обязательные поля
+        input1 = browser.find_element(By.CSS_SELECTOR, '.first_block .first')  # или '[placeholder="Input your first name"]' или ".first_block .form-control.first"
+        input1.send_keys("Ivan")
+        input2 = browser.find_element(By.CSS_SELECTOR, '[placeholder="Input your last name"]')  # или ".first_block .second" или ".first_block .form-control.second"
+        input2.send_keys("Petrov")
+        input3 = browser.find_element(By.CSS_SELECTOR, '[placeholder="Input your email"]')  # или ".first_block .third" или ".first_block .form-control.third"
+        input3.send_keys("ivanpetrov@mail.ru")
+
+        time.sleep(5)
+        # Отправляем заполненную форму
+        button = browser.find_element(By.CSS_SELECTOR, "button.btn")
+        button.click()
+
+        # Проверяем, что смогли зарегистрироваться
+        # ждем загрузки страницы
+        time.sleep(2)
+
+        # находим элемент, содержащий текст
+        welcome_text_elt = browser.find_element(By.TAG_NAME, "h1")
+        # записываем в переменную welcome_text текст из элемента welcome_text_elt
+        welcome_text = welcome_text_elt.text
+
+        # с помощью assert проверяем, что ожидаемый текст совпадает с текстом на странице сайта
+        self.assertEqual('Congratulations! You have successfully registered!', welcome_text), "NoSuchElementException"
+
+        # ожидание чтобы визуально оценить результаты прохождения скрипта
+        time.sleep(5)
+        # закрываем браузер после всех манипуляций
+        browser.quit()
+
+if __name__ == "__main__":
+    unittest.main()
